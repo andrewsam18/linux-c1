4ef7bed330e1acc7cea90565449ead126951e4d9
Andrew Samuel <andrewsam18@gmail.com>
2025-03-18 09:08:28 +0530

    link dark mode html css js where using  separate 3 files

---
+++ javascript/js example/ex19.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("icon");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        icon.src = "image/sun.png";
+    } else {
+        icon.src = "image/moon.png";
+    }
+}
