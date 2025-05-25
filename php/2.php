e745a1165dbd76e264120a0cc63f3c1cfd3ca22f
andrew samuel <andrewsam18@gmail.com>
2025-05-26 02:31:24 +0530

    diffrent between echoo and print

---
+++ php/2.php
@@ -0,0 +1,14 @@
+<?
+$msg="hii andrewsam";
+$dif="differ";
+echo"hello :".$msg,$dif;
+
+print  "hi :$msg, $dif";
+// PHP code demonstrating the use of echo and print
+// echo() and print() are both used to output strings in PHP, but they have some differences.
+// echo() can take multiple parameters, while print() can only take one.
+// echo() does not return a value, while print() returns 1. 
+// echo() is slightly faster than print() because it does not return a value.
+
+ ?>
+
