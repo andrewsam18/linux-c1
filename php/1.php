032f6186bf4271802a8d2f3cae91e74011250ff4
andrew samuel <andrewsam18@gmail.com>
2025-05-26 02:00:51 +0530

    define comments

--- php/1.php
+++ php/1.php
@@ -1,3 +1,12 @@
 <?php
-echo"hello world";
-?>
+echo"hello world\"escape\" my statement";
+
+print  "hi\"escape\" my statement";
+
+
+# print() -> Returns 1 (can be used in expressions)
+ # int print() --> Correct syntax: `print` returns an int, usually 1
+ # echo() --> Does not return a value (faster in some cases)
+ # void echo() --> Correct interpretation: `echo` does not return a value (not a function, hence no return type declaration like `void echo()`)
+ ?>
+
