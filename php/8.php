c3f48a5e395e298f7870e53c568091dcad32dad0
andrew samuel <andrewsam18@gmail.com>
2025-05-28 03:14:34 +0530

    const program

---
+++ php/8.php
@@ -0,0 +1,17 @@
+<?php
+function x(){
+$x =10;
+$y = 90;
+$x =$y;
+echo $x . "<br>";
+}
+x();
+/*
+This script defines a function x() that:
+- Initializes $x to 10 and $y to 90
+- Assigns the value of $y to $x
+- Outputs the value of $x followed by a line break
+
+Then, it calls the function x().
+*/
+?>
