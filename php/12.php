856c14716b64a629b4e7a6c64dc1ed768494f34f
andrew samuel <andrewsam18@gmail.com>
2025-06-01 02:04:35 +0530

    if ifelse esleif

---
+++ php/12.php
@@ -0,0 +1,22 @@
+<?php
+$x = 10;
+if($x<5){
+    echo " is larger";
+}
+elseif($x>12){//><
+    echo " is smaller";
+}
+else{
+    echo " valuesssss";
+}
+// if
+/* 
+This section demonstrates the use of if, elseif, and else statements in PHP.
+- The first if checks if $x is less than 5.
+- The elseif checks if $x is less than 12.
+- The else handles all other cases.
+*/
+//if-else
+// if-elseif
+//nested if
+?>
