b9384ec3367102a63c629f1fbc1545f413b72ddf
andrew samuel <andrewsam18@gmail.com>
2025-06-02 04:49:54 +0530

    switch case

--- php/14.php
+++ php/14.php
@@ -1,1 +1,18 @@
-php_check_syntax
+<?php
+$x = 10;
+
+switch ($x) {
+    case 1:
+        echo ("to 1");
+        break;
+    case 4:
+        echo( "to 4");
+        break;
+    case 10:
+        echo("correct option");
+        break;
+    default:
+        echo("final option");
+        break;
+}
+?>
