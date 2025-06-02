bcccbdc79b62fc476cb17d5c7bf962be67cdbf9c
andrew samuel <andrewsam18@gmail.com>
2025-06-03 03:51:58 +0530

    foreach part1

---
+++ php/16.php
@@ -0,0 +1,21 @@
+<?php
+$s = array("apple", "banana", "cherry");
+foreach($s as $element) {
+    echo "$element <br>";
+}
+ // foreach loop
+/**
+ * Iterates over each element in an array or object using a foreach loop.
+ * * This loop is specifically designed for iterating through collections, such as arrays or objects.
+ * * It automatically assigns each element to a variable, allowing easy access to the value.
+ * * Example:   
+ * foreach ($array as $value) {
+ * *     // Code to execute for each element
+ * * }
+ * * @var array $array The array or object to iterate over.
+ * * @var mixed $value The current value of the element being iterated.
+ * 
+ */
+
+
+?>
