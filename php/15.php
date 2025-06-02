2e3b9bf9695ca34338c84dc6d62af035fed96aaa
andrew samuel <andrewsam18@gmail.com>
2025-06-03 03:33:43 +0530

    fo loops

---
+++ php/15.php
@@ -0,0 +1,22 @@
+<?php
+for($n=1; $n<=10; $n++){
+    echo " $n <br>";
+}
+
+//for loop
+/**
+ * Iterates over a sequence of values using a for loop.
+ *
+ * Typically used to execute a block of code a specific number of times.
+ * The loop consists of an initialization, a condition, and an increment/decrement expression.
+ *
+ * Example:
+ * for ($i = 0; $i < $count; $i++) {
+ *     // Code to execute on each iteration
+ * }
+ *
+ * @var int $i Loop counter variable.
+ * @var int $count Number of iterations to perform.
+ */
+
+?>
