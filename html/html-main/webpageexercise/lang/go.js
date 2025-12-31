7eec38debdcc67b071c889a1eb0245615f125adc
andrew samuel <andrewsam18@gmail.com>
2025-12-31 07:24:45 +0530

    google

---
+++ html/html-main/webpageexercise/lang/go.js
@@ -0,0 +1,26 @@
+function googleTranslateElementInit(){
+    new google.translate.TranslateElement(
+        {
+            pageLanguage:'en',
+            includedLanguages:'en,ta,hi',
+            layout:google.translate.TranslateElement.InlineLayout.SIMPLE
+        },
+         'google_translate_element'
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
