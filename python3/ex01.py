6a7d7e2becb838b78e932013c7e019c1c7bf706d
andrew samuel <andrewsam18@gmail.com>
2026-02-26 01:39:35 +0530

    question 1

---
+++ python3/ex01.py
@@ -0,0 +1,25 @@
+"""name=input()
+age=input()
+print("my name is",name)
+print("my age is",age)
+
+name=input()
+age=input()
+address=input()
+print("my name is",name)
+print("my age is",age)
+print("my address is",address)
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=a*b*c
+e=a+b+c
+f=d/e
+print("the result is",f)"""
+name=input()
+score=int(input())
+department=input()
+print("my name is",name)
+print("my score is",score/10 ,"/10")
+print("my department is",department)
