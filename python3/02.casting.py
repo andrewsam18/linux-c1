53c92f38aacb0d8aeee8024e607b40ac274c789c
andrew samuel <andrewsam18@gmail.com>
2026-02-26 00:57:02 +0530

    casting

---
+++ python3/02.casting.py
@@ -0,0 +1,31 @@
+# Adding two integer variables
+# Prints the sum result
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# Joining two string values
+# Shows string concatenation
+a = "10"
+b = "20"
+c = a + b
+print(c)
+
+# Converting strings to integers
+# Prints correct numeric addition
+a = int("10")
+b = int("20")
+c = a + b
+print(c)
+
+#input from user
+a=input()
+b=input()
+c=a+b
+print(c)
+#user  int input ()change string to int
+a = int(input())
+b = int(input())
+c = a + b
+print(c)
