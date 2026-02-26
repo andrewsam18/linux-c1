4741a33dc0b8df19c2e251de11648f4f8268fdb3
andrew samuel <andrewsam18@gmail.com>
2026-02-27 01:49:26 +0530

    ifelse

---
+++ python3/03.ifelse.py
@@ -0,0 +1,20 @@
+"""#if else statement and comparison operators
+if (False):
+#if (True):
+    print("yes")
+else:
+    print("no")
+
+    print("win"=="win")
+    print("win"=="winn")
+#rcb="win"
+rcb="lose"
+if(rcb=="win"):
+    print("Ee sala cup namade")
+else:
+    print("valakam pola namkku cup ila")"""
+meghna=input("megana died or not?")
+if(meghna=="died"):
+    print("suriya meets priya")
+else:
+    print("suriya weds meghna")
